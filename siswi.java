acab00ab53bed80e2ea43ecf44bf1edd0c6ec7b2
nieshaRpL <141600352+nieshaRpL@users.noreply.github.com>
2024-01-09 19:36:18 +0700

    Add files via upload

---
+++ siswi.java
@@ -0,0 +1,11 @@
+
+package classobjek;
+
+public class siswi {
+    public static void main(String[] args) {
+        siswa s = new siswa();
+        System.out.println(s.nama);
+        System.out.println(s.getUsia());
+        System.out.println(s.Alamat);
+    }
+}
