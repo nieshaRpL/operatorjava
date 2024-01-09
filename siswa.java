8976dd5a96fad332e65ba0e48007c203c435e6ef
nieshaRpL <141600352+nieshaRpL@users.noreply.github.com>
2024-01-09 19:35:52 +0700

    Add files via upload

---
+++ siswa.java
@@ -0,0 +1,20 @@
+
+package classobjek;
+
+public class siswa {
+   public String nama = "Niesha";
+   private int usia = 14;
+   protected String Alamat = "kalimantan";
+   char jk ='p';
+   
+   //mutator methods (set usia)
+   public void setUsia(){
+       this.usia=usia;
+   }
+   
+   //assesor methods (get nilai)
+   public int getUsia(){
+       return usia;
+   }
+       
+    }
